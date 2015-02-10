d9e99a1ab9d75a153ee20b9c57a939e26c87ecce
xalvin <alvinKwong@hkours.com>
2015-02-10 11:18:27 +0800

    change on MapObject

--- src/com/jwetherell/augmented_reality/activity/MapObject.java
+++ src/com/jwetherell/augmented_reality/activity/MapObject.java
@@ -25,7 +25,8 @@ public class MapObject implements OnMapReadyCallback{
 	@Override
 	public void onMapReady(GoogleMap map) {
 		//plot route
-		if(map == null || points.size()==0) return;
+		map.setMyLocationEnabled(true);
+		if(map == null || points == null || points.size()==0) return;
 		int size = points.size();
 		GeoObj start = points.get(0).get(0);
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(
@@ -44,7 +45,30 @@ public class MapObject implements OnMapReadyCallback{
 			pl.width(10);
 			Log.v("map","add to map");
 			map.addPolyline(pl);
-			map.setMyLocationEnabled(true);
+		}
+	}
+	
+	public void plotRouteOnMap(GoogleMap map, ArrayList<ArrayList<GeoObj>> pts){
+		map.clear();
+		map.setMyLocationEnabled(true);
+		int size = pts.size();
+		GeoObj start = pts.get(0).get(0);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+				new LatLng(start.getLatitude(),start.getLongitude()),16));
+		//PolylineOptions pl = new PolylineOptions().geodesic(true);
+		for(int i =0; i<size;i++){
+			Log.v("map","plot line "+i);
+			PolylineOptions pl = new PolylineOptions().geodesic(true);
+			ArrayList<GeoObj> objList = pts.get(i);
+			Log.v("map","size "+objList.size());
+			for(GeoObj obj:objList){
+				Log.v("map","add GeoObj "+obj);
+				pl.add(new LatLng(obj.getLatitude(),obj.getLongitude()));
+			}
+			pl.color(COLORLIST[i%COLORLIST.length]);
+			pl.width(10);
+			Log.v("map","add to map");
+			map.addPolyline(pl);
 		}
 	}
 }
